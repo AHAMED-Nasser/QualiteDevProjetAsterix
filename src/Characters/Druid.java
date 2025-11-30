cfa743d7f3f06400c92651f0782f11a39475a1c2
nularyNasser <nahamed.sveil@gmail.com>
2025-11-30 23:24:10 +0100

    Création de la classe Druide

--- src/Characters/Druid.java
+++ src/Characters/Druid.java
@@ -1,4 +1,25 @@
 package src.Characters;
 
-public class Druid {
+import src.Interfaces.Fighter;
+import src.Interfaces.Leader;
+import src.Interfaces.Worker;
+
+public class Druid extends Character implements Worker, Leader, Fighter {
+
+    public Druid(String name, char sex, int height, int age, int strength, int stamina) {
+        super(name, sex, height, age, strength, stamina);
+    }
+
+    @Override
+    public void work() {}
+
+    @Override
+    public void lead(Character character) {
+        System.out.println(this.getName() + " donne un ordre à " + character.getName());
+    }
+
+    @Override
+    public void fight(Character character) {
+        System.out.println(this.getName() + " se battre contre " + character.getName());
+    }
 }
