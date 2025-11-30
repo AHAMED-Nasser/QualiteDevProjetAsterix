810a4765f2c49f220df03b5fa07383165d1f8b99
nularyNasser <nahamed.sveil@gmail.com>
2025-11-30 23:25:09 +0100

    Test de création des personnage via la FactoryCharacter. Problème d'affichage quand un personnage subit des dégats

--- Game.java
+++ Game.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import src.Characters.Blacksmith;
+import src.Characters.Character;
+import src.Characters.CharacterFactory;
+import src.Characters.Druid;
+import src.Enum.Faction;
+import src.Enum.Occupation;
 
 public class Game {
+    public static void main(String[] args) {
+        CharacterFactory factory = new CharacterFactory();
+
+        Character druide = factory.createCharacter(Faction.GAULOIS, Occupation.DRUID, "Panoramix");
+        Character forgeron = factory.createCharacter(Faction.GAULOIS, Occupation.FORGERON, "Cétautomatix");
+
+        System.out.println("--- Début de la simulation ---");
+
+        System.out.println(druide.getName());
+        System.out.println(forgeron.getName());
+
+        forgeron.takeDamage(5);
+    }
 }
