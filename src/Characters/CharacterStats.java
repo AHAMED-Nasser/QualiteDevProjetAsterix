b44033605bec40eab320d36b805760b361f1816d
nularyNasser <nahamed.sveil@gmail.com>
2025-11-30 23:22:57 +0100

    Class qui permet de manipuler facilement les statistiques des Charactere

--- src/Characters/CharacterStats.java
+++ src/Characters/CharacterStats.java
@@ -1,4 +1,34 @@
 package src.Characters;
 
 public class CharacterStats {
+    private int value;
+    private final int min;
+    private int max;
+
+    public CharacterStats(int value, int min, int max) {
+        this.value = this.clamp(value);
+        this.min = min;
+        this.max = max;
+    }
+
+    public int clamp(int value) {
+        return Math.max(this.min, Math.min(value, this.max));
+    }
+
+    public void add(int amount) {
+        this.value = this.clamp(this.value + amount);
+    }
+
+    public void increaseMax(int amount) {
+        this.max += amount;
+        this.value = this.clamp(this.value);
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+        this.value = this.clamp(this.value);
+    }
+
+    public int get() { return this.value; }
+    public int getMax() { return this.max; }
 }
