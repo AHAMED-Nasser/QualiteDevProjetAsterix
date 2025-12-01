470ee8fc5d7f1c47cbe9a22731c3888af5685af1
nularyNasser <nahamed.sveil@gmail.com>
2025-12-01 17:25:38 +0100

    Gestion des potions magique et création qui ne prend pas en compte les ingrédients

--- src/Characters/Gaulois/Druid.java
+++ src/Characters/Gaulois/Druid.java
@@ -1,11 +1,16 @@
 package src.Characters.Gaulois;
 
 import src.Characters.Character;
+import src.Characters.Statistics;
 import src.Interfaces.IFighter;
 import src.Interfaces.ILeader;
 import src.Interfaces.IWorker;
 
 public class Druid extends src.Characters.Character implements IWorker, ILeader, IFighter {
+
+    private final int MAX_MAGIC_POTION = 5;
+    private Statistics magicPotionBottle = new Statistics(0, 0, MAX_MAGIC_POTION);
+    private final Statistics magicPotionCauldron = new Statistics(0, 0, 1);
 
     public Druid(String name, char sex, int height, int age, int strength, int stamina) {
         super(name, sex, height, age, strength, stamina);
@@ -25,4 +30,19 @@ public class Druid extends src.Characters.Character implements IWorker, ILeader,
     public void fight(Character character) {
         System.out.println("[Druide] " + this.getName() + " se battre contre " + character.getName());
     }
+
+    public Statistics getMagicPotionBottle() { return this.magicPotionBottle; }
+    public void setMagicPotionBottle(Statistics nbMagicPotionBottle) { this.magicPotionBottle = nbMagicPotionBottle; }
+
+    public void createMagicPotion() {
+        this.magicPotionCauldron.add(1); // On créé un chaudron max
+        this.magicPotionBottle.add(5); // On créé cinq récipiant de potion magique
+
+        System.out.println("[Druide] " + this.getName() + " à créé une marmite de potion magique : " + this.magicPotionCauldron.get());
+        System.out.println(this.getNbMagicPotionBottle() + " récipiant de potion magique on été créé.");
+    }
+
+    public boolean hasMagicPotionCauldron() { return this.magicPotionCauldron.get() > 0; }
+    public boolean hasMagicPotionBottle() { return this.magicPotionBottle.get() > 0; }
+    public int getNbMagicPotionBottle() { return this.magicPotionBottle.get(); }
 }
