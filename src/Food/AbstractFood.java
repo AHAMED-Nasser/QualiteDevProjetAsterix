a95dfd3000daccd3ab3bf1afbf792a7e5bfbc4f8
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2025-12-07 19:09:30 +0100

    Maj création de la classe AbstractFood

--- src/Food/AbstractFood.java
+++ src/Food/AbstractFood.java
@@ -1,5 +1,25 @@
 package src.Food;
 
+import src.Statistics;
+
 public abstract class AbstractFood {
+
+    private String name;
+    private Statistics foodNutrition;
+    private FoodType foodType;
+    private FoodFreshness foodFreshness;
+
+    public AbstractFood(String name, Statistics foodNutrition, FoodType foodType, FoodFreshness foodFreshness) {
+        this.name = name;
+        this.foodNutrition = foodNutrition;
+        this.foodType = foodType;
+        this.foodFreshness = foodFreshness;
+    }
+
+    // Getter
+    public String getName() { return this.name;}
+    public Statistics getFoodNutrition() { return this.foodNutrition; }
+    public FoodType getFoodType() { return this.foodType; }
+    public FoodFreshness getFoodFreshness() { return this.foodFreshness; }
 
 }
