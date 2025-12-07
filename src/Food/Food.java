bbc0707d92b89a5f27e41880d27ccef092b63c09
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2025-12-07 19:41:11 +0100

    2 FIX (Changement de type foodNutrition) (Retirage de Abstract)

--- src/Food/Food.java
+++ src/Food/Food.java
@@ -2,14 +2,14 @@ package src.Food;
 
 import src.Statistics;
 
-public abstract class AbstractFood {
+public class Food {
 
     private String name;
-    private Statistics foodNutrition;
+    private int foodNutrition;
     private FoodType foodType;
     private FoodFreshness foodFreshness;
 
-    public AbstractFood(String name, Statistics foodNutrition, FoodType foodType, FoodFreshness foodFreshness) {
+    public Food(String name, int foodNutrition, FoodType foodType, FoodFreshness foodFreshness) {
         this.name = name;
         this.foodNutrition = foodNutrition;
         this.foodType = foodType;
@@ -18,7 +18,7 @@ public abstract class AbstractFood {
 
     // Getter
     public String getName() { return this.name;}
-    public Statistics getFoodNutrition() { return this.foodNutrition; }
+    public int getFoodNutrition() { return this.foodNutrition; }
     public FoodType getFoodType() { return this.foodType; }
     public FoodFreshness getFoodFreshness() { return this.foodFreshness; }
 
