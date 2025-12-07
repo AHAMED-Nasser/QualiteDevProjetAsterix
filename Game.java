01e9629a13c71f3ede59cc5c1cc7ea347dcd50be
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2025-12-07 19:40:01 +0100

    Test du factory

--- Game.java
+++ Game.java
@@ -2,27 +2,39 @@ import src.Characters.Character;
 import src.Characters.CharacterFactory;
 import src.Enum.Faction;
 import src.Enum.Occupation;
+import src.Food.Food;
+import src.Food.FoodFactory;
+import src.Food.FoodType;
 import src.Interfaces.IFighter;
 import src.Interfaces.ILeader;
 import src.Interfaces.IWorker;
 
 public class Game {
     public static void main(String[] args) {
-        CharacterFactory factory = new CharacterFactory();
 
-        Character druide = factory.createCharacter(Faction.GAULOIS, Occupation.DRUID, "Panoramix");
-        Character forgeron = factory.createCharacter(Faction.GAULOIS, Occupation.FORGERON, "Cétautomatix");
-        Character marchand = factory.createCharacter(Faction.GAULOIS, Occupation.MARCHAND, "Ordralfabétix");
-        Character aubergiste = factory.createCharacter(Faction.GAULOIS, Occupation.AUBERGISTE, "Odalix");
+        // =============== CHARACTER ================
+        CharacterFactory characterFactory = new CharacterFactory();
 
-        Character legionnaire = factory.createCharacter(Faction.ROMAIN, Occupation.LEGIONNAIRE, "Pamplemus");
-        Character prefet = factory.createCharacter(Faction.ROMAIN, Occupation.PREFET, "Bonusmalus");
-        Character general = factory.createCharacter(Faction.ROMAIN, Occupation.GENERAL, "Jules César");
+        Character druide = characterFactory.createCharacter(Faction.GAULOIS, Occupation.DRUID, "Panoramix");
+        Character forgeron = characterFactory.createCharacter(Faction.GAULOIS, Occupation.FORGERON, "Cétautomatix");
+        Character marchand = characterFactory.createCharacter(Faction.GAULOIS, Occupation.MARCHAND, "Ordralfabétix");
+        Character aubergiste = characterFactory.createCharacter(Faction.GAULOIS, Occupation.AUBERGISTE, "Odalix");
 
-        Character lycanthrop = factory.createCharacter(Faction.CREATURE_FANTASTIQUE, Occupation.LYCANTHROPE, "Prolix");
+        Character legionnaire = characterFactory.createCharacter(Faction.ROMAIN, Occupation.LEGIONNAIRE, "Pamplemus");
+        Character prefet = characterFactory.createCharacter(Faction.ROMAIN, Occupation.PREFET, "Bonusmalus");
+        Character general = characterFactory.createCharacter(Faction.ROMAIN, Occupation.GENERAL, "Jules César");
+
+        Character lycanthrop = characterFactory.createCharacter(Faction.CREATURE_FANTASTIQUE, Occupation.LYCANTHROPE, "Prolix");
+
+        // =============== FOOD =================
+        FoodFactory foodFactory = new FoodFactory();
+
+        Food sanglier = foodFactory.createFood(FoodType.WILD_BOAR);
+
 
         System.out.println("--- Début de la simulation ---");
 
+        // CHARACTER
         ((IWorker) druide).work();
         ((IWorker) forgeron).work();
         ((IWorker) marchand).work();
@@ -35,5 +47,8 @@ public class Game {
         ((ILeader) general).lead(legionnaire);
 
         ((IFighter) lycanthrop).fight(druide);
+
+        // FOOD
+        System.out.println(sanglier.getName());
     }
 }
