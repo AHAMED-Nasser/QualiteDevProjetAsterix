40bbb04c18453eb677fca5e16a0c12279c29f5ac
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2025-12-11 00:20:07 +0100

    MAJ tout les méthode du chef de clan son ici pour le moment

--- src/Place/SafePlace.java
+++ src/Place/SafePlace.java
@@ -1,4 +1,102 @@
 package src.Place;
 
-public class SafePlace {
+import src.Characters.Character;
+import src.Food.Food;
+import src.Place.Enum.TypePlace;
+import src.Place.Interface.ISafePlace;
+import src.Statistics;
+
+import java.util.List;
+
+public class SafePlace extends Place implements ISafePlace {
+
+    private Character clanChief;
+
+    public SafePlace(String name, int surface, TypePlace typePlace, List<Character> characterList, List<Food> foodList, Character clanChief) {
+        super(name, surface, typePlace, characterList, foodList);
+        this.clanChief = clanChief;
+    }
+
+    @Override
+    public void displayPlaceInfo() {
+        System.out.println("<----------- INFORMATION DU LIEU: " + this.getName() + " ------------>");
+        System.out.println("Nom: " + this.getName());
+        System.out.println("Surface: " + this.getSurface() + "m²");
+        System.out.println("Type de village: " + this.getTypePlace());
+    }
+
+    @Override
+    public void displayCharacter() {
+        System.out.println("<---- Personnage présent dans le lieu: " + this.getName() + " ---->");
+        for (Character character : this.getCharacterList()) {
+            System.out.println("-> " + character.getName());
+        }
+    }
+
+    @Override
+    public void displayCharacterMinInfo() {
+        System.out.println();
+        System.out.println("<---- Information des personnages ---->");
+        for (Character character : this.getCharacterList()) {
+            character.displayMinInformation();
+        }
+    }
+
+    @Override
+    public void displayCharacterFullInfo() {
+        System.out.println();
+        System.out.println("<---- Information des personnages ---->");
+        for (Character character : this.getCharacterList()) {
+            character.displayFullInformation();
+        }
+    }
+
+    @Override
+    public void displayFood() {
+        System.out.println("<---- Aliment présent dans le lieu: " + this.getName() + " ---->");
+        for (Food food : this.getFoodList()) {
+            System.out.println("-> " + food);
+        }
+    }
+
+    @Override
+    public void addCharacter(Character character) {
+        this.getCharacterList().add(character);
+    }
+
+    @Override
+    public void removeCharacter(Character character) {
+        this.getCharacterList().remove(character);
+    }
+
+    @Override
+    public void healAllCharacters() {
+        for (Character character : this.getCharacterList()) {
+            character.heal(100);
+        }
+    }
+
+    @Override
+    public void healOneCharacters(Character character) {
+        character.heal(100);
+    }
+
+    @Override
+    public void addFood(Food food) {
+        this.getFoodList().add(food);
+    }
+
+    @Override
+    public void feedAllCharacters() {
+        System.out.println("Le chef de clan à nourrit tout le monde.");
+        for (Character character : this.getCharacterList()) {
+            character.setHunger(new Statistics(100, 0, 100));
+        }
+    }
+
+    @Override
+    public void feedOneCharacters(Character character) {
+        System.out.println("Le chef de clan à nourrit " + character.getName());
+        character.setHunger(new Statistics(100, 0, 100));
+    }
 }
