a1df17c4a6a965745a38c45d07a652cbc41857c1
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2025-12-11 00:17:38 +0100

    Type de place possible

--- src/Place/Enum/TypePlace.java
+++ src/Place/Enum/TypePlace.java
@@ -1,4 +1,10 @@
 package src.Place.Enum;
 
 public enum TypePlace {
-}
+    GAULS_VILLAGE, // Village gaulois
+    ROMAN_FORTIFIED_CAMP, // Camp retranché romain
+    ROMAN_CITY, // Ville romaine
+    GALLO_ROMAN_VILLAGE, // Bourgade gallo-romaine
+    ENCLOSURE, // Enclos
+    BATTLEFIELD // Champ de bataille
+    }
