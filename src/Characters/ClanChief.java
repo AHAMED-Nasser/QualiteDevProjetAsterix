b2ee0d343ba98001d2675c53e00cd1b3f0b8b71b
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2025-12-11 00:15:13 +0100

    Classe chef de clan, qui n'est pas complet pour l'instant

--- src/Characters/ClanChief.java
+++ src/Characters/ClanChief.java
@@ -1,4 +1,13 @@
 package src.Characters;
 
-public class ClanChief {
+import src.Enum.Character.Faction;
+import src.Food.Food;
+import src.Interfaces.IClanChief;
+import src.Characters.Character;
+
+public class ClanChief extends Character implements IClanChief {
+
+    public ClanChief(String name, char sex, int height, int age, int strength, int stamina, Faction faction) {
+        super(name, sex, height, age, strength, stamina, faction);
+    }
 }
