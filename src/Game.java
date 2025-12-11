333f471242702e4d5023e7a55953f4bef3ae1855
nularyNasser <nahamed.sveil@gmail.com>
2025-12-11 13:30:32 +0100

    FIX test des classes

--- src/Game.java
+++ src/Game.java
@@ -2,38 +2,34 @@ package src;
 
 import src.Characters.Character;
 import src.Characters.CharacterFactory;
+import src.Characters.CreatePopulation;
 import src.Enum.Character.Faction;
 import src.Enum.Character.Occupation;
 import src.Food.Food;
 import src.Food.FoodFactory;
 import src.Enum.Food.FoodType;
 import src.Enum.Place.TypePlace;
+import src.Food.StockFood;
 import src.Place.PlaceFactory;
 import src.Place.SafePlace;
+
+import java.util.List;
+import java.util.Map;
 
 public class Game {
 
     public static void main(String[] var0) {
 
-        // Characters
-        CharacterFactory var1 = new CharacterFactory();
-        Character var2 = var1.createCharacter(Faction.GAULS, Occupation.DRUID, "Panoramix");
-        Character var3 = var1.createCharacter(Faction.GAULS, Occupation.FORGERON, "Cétautomatix");
-        Character var4 = var1.createCharacter(Faction.GAULS, Occupation.MARCHAND, "Ordralfabétix");
-        Character var5 = var1.createCharacter(Faction.GAULS, Occupation.AUBERGISTE, "Odalix");
-        Character var6 = var1.createCharacter(Faction.ROMAN, Occupation.LEGIONNAIRE, "Pamplemus");
-        Character var7 = var1.createCharacter(Faction.ROMAN, Occupation.PREFET, "Bonusmalus");
-        Character var8 = var1.createCharacter(Faction.ROMAN, Occupation.GENERAL, "Jules César");
-        Character var9 = var1.createCharacter(Faction.FANTASTIC_CREATURE, Occupation.LYCANTHROPE, "Prolix");
+        // Create random population
+        CreatePopulation populator = new CreatePopulation();
+        List<Character> allCharacters = populator.generateSimulationPopulation(10, 10, 9);
 
-        Character chef_clan = var1.createCharacter(Faction.GAULS, Occupation.CLAN_CHIEF, "Nasser");
+        // Create clan chief
+        CharacterFactory factory = new CharacterFactory();
+        Character chef_clan = factory.createCharacter(Faction.GAULS, Occupation.CLAN_CHIEF, "Nasser");
 
-        // Food
-        FoodFactory foodFactory = new FoodFactory();
-
-        Food sanglier = foodFactory.createFood(FoodType.WILD_BOAR);
-        Food poisson_pas_frais = foodFactory.createFood(FoodType.NOT_FRESH_FISH);
-        Food fraise = foodFactory.createFood(FoodType.STRAWBERRY);
+        Character unRomain = factory.createCharacter(Faction.ROMAN, Occupation.LEGIONNAIRE, "Un Romain");
+        Character unGaulois = factory.createCharacter(Faction.GAULS, Occupation.FORGERON, "Un Gaulois");
 
         // PLACE
 
@@ -43,17 +39,17 @@ public class Game {
 
         System.out.println("--- Début de la simulation ---");
 
-        pydertale.addCharacter(var2);
-        pydertale.addCharacter(var3);
+        for (int i = 0; i < allCharacters.size(); i++) {
+            System.out.println(allCharacters.get(i).getName());
+        }
 
-        pydertale.displayCharacterMinInfo();
 
-        var2.setHunger(new Statistics(50, 0, 100));
+        pydertale.displayFood();
 
-        pydertale.displayCharacterMinInfo();
-
-        pydertale.feedOneCharacters(var2);
-
-        pydertale.displayCharacterMinInfo();
+//
+//        pydertale.displayCharacter();
+//        pydertale.displayCharacterMinInfo();
+//
+//        pydertale.displayPlaceInfo();
     }
 }
